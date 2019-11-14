fae058b0648178b54adc417a677820399787566a
youvovk <yuriy3vovk@gmail.com>
2019-11-14 11:07:02 +0200

    fix text indent add main back

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import './main.scss';
 function App() {
   return (
     <>
-    <div className="footer-background"></div>
       <div className="background">
-
         <div className="container">
 
           {/* <header className="header">
@@ -19,48 +17,50 @@ function App() {
             <input type="button" value="Register Now" className="register" />
           </header> */}
 
-          <div className="main-wrapper">
-            <main className="main">
-              <p className="text text_29px">limited time offer</p>
-                <p className="text text__extra-bold text_43px wrapper">
-                  FIRST DEPOSIT BONUS: 200% UP TO £50 + 200 BONUS 
-                </p>
-              <p className="text text__extra-bold text_29px p_indent">+40 free SPINS</p>
+            <div className="main-wrapper">
+              <main className="main">
+                <p className="text text_29px">limited time offer</p>
+                  <p className="text text__extra-bold text_43px wrapper">
+                    FIRST DEPOSIT BONUS: 200% UP TO £50 + 200 BONUS 
+                  </p>
+                <p className="text text__extra-bold text_29px p_indent">+40 free SPINS</p>
 
-              <a href="#" className="register register_big">Register Now</a>
-            </main>
+                <a href="#" className="register register_big">Register Now</a>
+              </main>
+            </div>
           </div>
+      </div>
 
-          <div className="footer-wrapper">
-            <footer className="footer">
-              <div className="footer__top">
-                  <img src="./images/visa.png" alt="" />
-                  <img src="./images/master-card.png" alt="" />
-                  <img src="./images/skrill.png" alt="" />
-                  <img src="./images/pay.png" alt="" />
-                  <img src="./images/visa.png" alt="" />
-                  <img src="./images/master-card.png" alt="" />
-                  <img src="./images/skrill.png" alt="" />
-                  <img src="./images/pay.png" alt="" />
-                  <img src="./images/visa.png" alt="" />
-                  <img src="./images/master-card.png" alt="" />
-                  <img src="./images/skrill.png" alt="" />
-              </div>
+      <div className="footer-background">
+        <div className="footer-wrapper">
+          <footer className="footer">
+            <div className="footer__top">
+                <img src="./images/visa.png" alt="" />
+                <img src="./images/master-card.png" alt="" />
+                <img src="./images/skrill.png" alt="" />
+                <img src="./images/pay.png" alt="" />
+                <img src="./images/visa.png" alt="" />
+                <img src="./images/master-card.png" alt="" />
+                <img src="./images/skrill.png" alt="" />
+                <img src="./images/pay.png" alt="" />
+                <img src="./images/visa.png" alt="" />
+                <img src="./images/master-card.png" alt="" />
+                <img src="./images/skrill.png" alt="" />
+            </div>
 
-              <div className="footer__bottom">
-                  <img src="./images/skrill.png" alt="" />
-                  <img src="./images/pay.png" alt="" />
-                  <img src="./images/visa.png" alt="" />
-                  <img src="./images/master-card.png" alt="" />
-                  <img src="./images/skrill.png" alt="" />
-                  <img src="./images/pay.png" alt="" />
-                  <img src="./images/visa.png" alt="" />
-              </div>
-            </footer>
-          </div>
+            <div className="footer__bottom">
+                <img src="./images/skrill.png" alt="" />
+                <img src="./images/pay.png" alt="" />
+                <img src="./images/visa.png" alt="" />
+                <img src="./images/master-card.png" alt="" />
+                <img src="./images/skrill.png" alt="" />
+                <img src="./images/pay.png" alt="" />
+                <img src="./images/visa.png" alt="" />
+            </div>
+          </footer>
         </div>
       </div>
-      </>
+    </>
   );
 }
 
