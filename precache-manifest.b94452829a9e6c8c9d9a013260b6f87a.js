9cb2aa8d709cdf6e51a2213798768e309d843ee1
youvovk <yuriy3vovk@gmail.com>
2019-11-19 17:36:37 +0200

    Updates

---
+++ precache-manifest.b94452829a9e6c8c9d9a013260b6f87a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "899e4042c8f6d7f654cd201426162fb6",
+    "url": "/sports-landing/index.html"
+  },
+  {
+    "revision": "f7f4d58f637efd69eb22",
+    "url": "/sports-landing/static/css/main.ee56d7ee.chunk.css"
+  },
+  {
+    "revision": "9e6517bb1abd52ec2e06",
+    "url": "/sports-landing/static/js/2.f1cbc75e.chunk.js"
+  },
+  {
+    "revision": "f7f4d58f637efd69eb22",
+    "url": "/sports-landing/static/js/main.d9550f02.chunk.js"
+  },
+  {
+    "revision": "346523549d8e552a99c2",
+    "url": "/sports-landing/static/js/runtime-main.8e0cd547.js"
+  },
+  {
+    "revision": "724e1d75d07b4608eb3607ddcadd8d2a",
+    "url": "/sports-landing/static/media/back-footer.724e1d75.jpg"
+  },
+  {
+    "revision": "a7150123169263982881be42d0aae9de",
+    "url": "/sports-landing/static/media/back-main.a7150123.jpg"
+  }
+]);
