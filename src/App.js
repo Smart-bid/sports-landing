1da1a84f894120de0339ade4a5aa0a81e70a83b7
youvovk <yuriy3vovk@gmail.com>
2019-11-19 17:56:11 +0200

    fix header

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ export class App extends React.Component {
     return (
       <>
         <div className="background">
-          <div className="container">
-            <header className={`header ${headerBack && 'header__background'}`}>
+          <div className={`header-wrapper ${headerBack && 'header-wrapper_background'}`}>
+            <header className="header">
               <div className="header__logo-wrapper">
                 <p className="header__logo">logo</p>
                 <p className="header__logo_bottom">lorem ipsum</p>
@@ -37,6 +37,9 @@ export class App extends React.Component {
 
               <input type="button" value="Register Now" className="register" />
             </header>
+          </div>
+
+          <div className="container">
 
             <div className="main-wrapper">
               <main className="main">
